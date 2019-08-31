7270e7ddc1c1cfbbc9c74a7354dd4b9a2a5f64ce
zl <jjjachyty@163.com>
2019-08-31 22:15:56 +0800

    修改dapp第一次加载不出地址

--- lib/src/dapp/dapp_full.dart
+++ lib/src/dapp/dapp_full.dart
@@ -14,7 +14,7 @@ const kAndroidUserAgent =
     'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Mobile Safari/537.36';
 
 // String selectedUrl = 'http://192.168.1.11:8080';
-  // var scaffoldKey = GlobalKey<ScaffoldState>();
+// var scaffoldKey = GlobalKey<ScaffoldState>();
 
 class DappFullPage extends StatefulWidget {
   Dapp app;
@@ -25,24 +25,23 @@ class DappFullPage extends StatefulWidget {
 
 class _DappFullPageState extends State<DappFullPage> {
   final flutterWebViewPlugin = FlutterWebviewPlugin();
-    StreamSubscription<WebViewStateChanged> _onStateChanged;
+  StreamSubscription<WebViewStateChanged> _onStateChanged;
 
   Dapp app;
   _DappFullPageState(this.app);
 
-
-   @override
+  @override
   void initState() {
     super.initState();
-    _onStateChanged = flutterWebViewPlugin.onStateChanged.listen((WebViewStateChanged state) {
-  if (mounted) {
-    if (state.type == WebViewState.finishLoad) {
-      flutterWebViewPlugin.evalJavascript(
-          "window.localStorage.setItem('address','"+address[0].val+"');"
-      );
-    }
-  }
-});
+    _onStateChanged =
+        flutterWebViewPlugin.onStateChanged.listen((WebViewStateChanged state) {
+      if (mounted) {
+        // if (state.type == WebViewState.finishLoad) {
+        flutterWebViewPlugin.evalJavascript(
+            "window.localStorage.setItem('address','" + address[0].val + "');");
+        // }
+      }
+    });
   }
 
   @override
@@ -55,23 +54,23 @@ class _DappFullPageState extends State<DappFullPage> {
 
   @override
   Widget build(BuildContext context) {
-    return  WebviewScaffold(
-      url: app.homePage,
-      javascriptChannels: getJsChannel(),
-      //  appBar: AppBar(centerTitle: true,backgroundColor: Colors.transparent,title: Text("APP2MHC",style: TextStyle(color: Colors.green),),elevation: 0,),
-      withLocalStorage: true,
-      withJavascript: true,      
-      initialChild: 
-      Builder(builder: (context){
-        dappContext = context;
-        return   
-        Container(
-        color: Colors.green,
-        child: Center(
-          child: Text("广告位招租,详询客服",style: TextStyle(color: Colors.white),),
-        ),
-      );
-     })
-      
-    );}
+    return WebviewScaffold(
+        url: app.homePage,
+        javascriptChannels: getJsChannel(),
+        //  appBar: AppBar(centerTitle: true,backgroundColor: Colors.transparent,title: Text("APP2MHC",style: TextStyle(color: Colors.green),),elevation: 0,),
+        withLocalStorage: true,
+        withJavascript: true,
+        initialChild: Builder(builder: (context) {
+          dappContext = context;
+          return Container(
+            color: Colors.green,
+            child: Center(
+              child: Text(
+                "广告位招租,详询客服",
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+          );
+        }));
+  }
 }
